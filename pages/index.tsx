1dff811a3683c37a62072ee76c1b0b47a1c7f4a2
Mavahu <martinvhubert@gmail.com>
2022-03-14 16:52:28 +0100

    fixed playlist bottom padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { getSession, GetSessionParams } from 'next-auth/react'
-import Center from '../components/Center'
-import Player from '../components/Player'
-import Sidebar from '../components/Sidebar'
+import { getSession, GetSessionParams } from 'next-auth/react';
+import Center from '../components/Center';
+import Player from '../components/Player';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
@@ -15,17 +15,17 @@ export default function Home() {
         <Player />
       </div>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps(
   context: GetSessionParams | undefined
 ) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   return {
     props: {
       session,
     },
-  }
+  };
 }
